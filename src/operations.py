5301b54338035e8095293a27709b6324f194d1ad
Dexter <gilmuher93@gmail.com>
2019-01-31 22:13:20 -0500

    Operations and tests added.
    math.py module was renamed due to name conflict.

---
+++ src/operations.py
@@ -0,0 +1,24 @@
+
+
+def add(a, b):
+    """ Returns the result of adding the parameters. """
+
+    return a + b
+
+
+def subtract(a, b):
+    """ Returns the result of subtracting the first parameter by the second. """
+
+    return a - b
+
+
+def multiply(a, b):
+    """ Returns the result of multiplying the parameters. """
+
+    return a * b
+
+
+def divide(a, b):
+    """ Returns the result of dividing the first parameter by the second. """
+
+    return a // b
---
+++ tests/operations_test.py
@@ -0,0 +1,44 @@
+
+from src import operations
+
+
+def test_add():
+    assert operations.add(1, 2) == 3
+    assert operations.add(-1, 2) == 1
+    assert operations.add(-1, -2) == -3
+    assert operations.add(0, 1) == 1
+
+
+def test_subtract():
+    assert operations.subtract(1, 2) == -1
+    assert operations.subtract(-1, 2) == -3
+    assert operations.subtract(-1, -2) == 1
+    assert operations.subtract(0, 1) == -1
+
+
+def test_multiply():
+    assert operations.multiply(1, 2) == 2
+    assert operations.multiply(-1, 2) == -2
+    assert operations.multiply(-1, -2) == 2
+    assert operations.multiply(0, 1) == 0
+
+
+def test_divide():
+    assert operations.divide(2, 1) == 2
+    assert operations.divide(0, 2) == 0
+    assert operations.divide(1, -2) == -1
+    assert operations.divide(4, 3) == 1
+
+
+def main():
+
+    test_add()
+    test_subtract()
+    test_multiply()
+    test_divide()
+
+    print("All operations tests finished correctly.")
+
+
+if __name__ == "__main__":
+    main()
